e2659c254ac608d8cf98db6e203da17e4e378a1e
rchtsang <rchtsang@ucdavis.edu>
2024-01-11 10:06:59 -0800

    fix download link for support

--- Casks/ucd-eec172-cc3200-support.rb
+++ Casks/ucd-eec172-cc3200-support.rb
@@ -1,8 +1,8 @@
 cask "ucd-eec172-cc3200-support" do
   version "1.5.0"
-  sha256 "2c17dff6ed33c4fa5e728648851193f08bf4b67f93c64b37fff339688ebaa4fd"
+  sha256 "bec605e31bbcb96ba3dab024f1ba8beda0fed9908f70bc9d3d8095307c528534"
 
-  url "https://drive.google.com/uc?export=download&id=1bOM-ecZueOonM0ARWNGvJVbUaB3HthLq&authuser=0&confirm=t"
+  url "https://github.com/rchtsang/ucd-eec172-public/raw/main/files/cc3200-support.zip"
   name "CC3200 Support Files"
   desc "SDK 1.5.0 and ServicePack 1.0.1.15-2.15.0.1 for CC3200"
   homepage "https://www.ti.com/tool/CC3200SDK"
