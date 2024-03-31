6e5395417546704a5a40d1ba91c93d5352787905
rchtsang <rchtsang@ucdavis.edu>
2024-03-31 10:20:00 -0700

    change repo ownership

--- Casks/ucd-eec172-cc3200-support.rb
+++ Casks/ucd-eec172-cc3200-support.rb
@@ -2,7 +2,7 @@ cask "ucd-eec172-cc3200-support" do
   version "1.5.0"
   sha256 "bec605e31bbcb96ba3dab024f1ba8beda0fed9908f70bc9d3d8095307c528534"
 
-  url "https://github.com/rchtsang/ucd-eec172-public/raw/main/files/cc3200-support.zip"
+  url "https://github.com/ucd-eec172/ucd-eec172.github.io/raw/main/files/cc3200-support.zip"
   name "CC3200 Support Files"
   desc "SDK 1.5.0 and ServicePack 1.0.1.15-2.15.0.1 for CC3200"
   homepage "https://www.ti.com/tool/CC3200SDK"
--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 ## How do I install these formulae?
 
-`brew install rchtsang/ucd-eec172/<formula>`
+`brew install ucd-eec172/ucd-eec172/<formula>`
 
-Or `brew tap rchtsang/ucd-eec172` and then `brew install <formula>`.
+Or `brew tap ucd-eec172/ucd-eec172` and then `brew install <formula>`.
 
 ## Documentation
 
